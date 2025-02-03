0cd20fcdf3e3ead95a1abd168bae6f2439fbd7f1
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-02-03 22:31:25 +0530

    Add SideBar on admin page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,13 +2,33 @@ import React,{useContext} from 'react'
 import Login from '../src/pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import DoctorsList from './pages/Admin/DoctorsList';
+import AddDoctor from './pages/Admin/AddDoctor';
+import AllAppointments from './pages/Admin/AllAppointments';
+import Dashboard from './pages/Admin/Dashboard';
 const App = () => {
 
   const {aToken} = useContext(AdminContext)
   return aToken? (
-    <div>
+    <div className='bg-[#F8F9FD]'>
       
       <ToastContainer/>
+      <Navbar/>
+      <div className='flex items-start'>
+        <Sidebar/>
+        <Routes>
+          <Route
+          path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={<Dashboard/>} />
+          <Route path='/all-appointments' element={<AllAppointments/>} />
+          <Route path='/add-doctor' element={<AddDoctor/>}/>
+          <Route path='/doctor-list' element={<DoctorsList/>}/>
+          
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
