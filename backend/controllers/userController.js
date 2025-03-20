4af559c0aa9d7bde7b842779d75937f5d715ea86
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-03-20 15:55:30 +0530

    Get Use- profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,5 +77,19 @@ const loginUser = async(req,res) => {
         res.json({success: false, message:error.message})
     }
 }
+//API to get user profile data
+const getProfile = async (req, res) => {
+    
+    try {
+        const{ userId } = req.body
+        const userData = await userModel.findById(userId).select(' -password ')
 
-export {registerUser,loginUser}
+        res.json({success:true, userData})
+    }catch (error){
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+}
+
+
+export {registerUser,loginUser, getProfile}
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -10,10 +10,8 @@ const AppContextProvider = (props) => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const [doctors,setDoctors] = useState([])
-    const value = {
-        doctors,
-        currencySymbol
-    }
+    const [token,setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token'):false)
+    
 
     const getDoctorsData = async () => {
         try {
@@ -28,6 +26,14 @@ const AppContextProvider = (props) => {
             toast.error(error.message)
         }
     }
+
+    const value = {
+        doctors,
+        currencySymbol,
+        token,setToken,
+        backendUrl
+    }
+
     useEffect(()=>{
         getDoctorsData()
     },[])
