983a1fa192d657c19cbfcd97fe84a1a994be871d
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-03-22 07:15:43 +0530

    Update user PRofile data except image in nav bar

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -11,7 +11,7 @@ const AppContextProvider = (props) => {
 
     const [doctors,setDoctors] = useState([])
     const [token,setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token'):false)
-    
+    const [userData,setUserData] = useState(false)
 
     const getDoctorsData = async () => {
         try {
@@ -26,17 +26,45 @@ const AppContextProvider = (props) => {
             toast.error(error.message)
         }
     }
+    
+    const loadUserProfileData = async () => {
+        try {
+            const { data } = await axios.get(backendUrl + '/api/user/get-profile', {headers: {token}})
+            if(data.success){
+                setUserData(data.userData)
+                
+            } else{
+                
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
 
     const value = {
         doctors,
         currencySymbol,
         token,setToken,
-        backendUrl
+        backendUrl,
+        userData,setUserData,
+        loadUserProfileData
     }
 
     useEffect(()=>{
         getDoctorsData()
     },[])
+
+
+    useEffect(() => {
+        if(token) {
+            loadUserProfileData()
+        } else {
+            setUserData(false)
+        }
+    },[token])
     return(
         <AppContext.Provider value={value}>
             {props.children}
--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,32 +1,61 @@
-import React, {useState} from 'react'
-import {assets} from '../assets/assets'
+import React, {useContext, useState} from 'react'
+import { AppContext } from '../context/AppContext'
+import { assets } from '../assets/assets'
+import axios from 'axios'
+
 const MyProfile = () => {
 
-  const [userData,setUserData] = useState({
-    name:"Kushal Tyagi",
-    image: assets.profile_pic,
-    email: 'kushaltyagi2001@gmail.com',
-    phone: '+91 8795865441',
-    address: {
-      line1:'501,5th Floor , Avinya Heights',
-      line2: 'sector 31, Noida, India'
-    },
-    gender: 'Male',
-    dob: '2000-01-20'
-  })
+  const {userData, setUserData, token, backendUrl, loadUserProfileData} = useContext(AppContext)
 
   const [isEdit,setIsEdit] = useState(false)
-  return (
-    <div className='max-w-lg flex flex-col gap-2 text-sm'>
-      <img 
-      className='w-36 rounded'
-      src={userData.image} alt=""/>
+  const [image, setImage] = useState(false)
 
+  const updateUserProfileData = async () => {
+    try {
+      const formData  = new FormData()
+
+      formData.append('name', userData.name)
+      formData.append('phone',userData.phone)
+      formData.append('address',JSON.stringify(userData.address))
+      formData.append('gender',userData.gender)
+      formData.append('dob',userData.dob)
+
+      image && formData.append('image',image)
+
+      const { data } = await axios.post(backendUrl + '/api/user/update-profile',formData,{headers:{token}})
+
+      if(data.success){
+        toast.success(data.message)
+        await loadUserProfileData()
+        setIsEdit(false)
+        setImage(false)
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
+  return userData &&  (
+    <div className='max-w-lg flex flex-col gap-2 text-sm'>
+
+      {
+        isEdit
+        ? <label htmlFor="image">
+            <div className="inline-block relative cursor-pointer">
+              <img className="w-36 rounded opacity-75" src={image ? URL.createObjectURL(image): userData.image} alt=""/>
+              <img className="w-10 absolute bottom-12 right-12" src={image ? '' : assets.upload_icon} alt=""/>
+            </div>
+            <input onChange={(e)=>setImage(e.target.files[0])} type="file" id="image" hidden/>
+        </label>
+        : <img className='w-36 rounded' src={userData.image} alt=""/>
+      }
       {
         isEdit
         ? <input 
         className='bg-gray-50 text-3xl font-medium max-w-60 mt-4'
-        type='text' value={userData.name} onChange={e => setUserData(prev => ({...prev,name:e.target.value}))}/>
+        type='text' value={userData.name} onChange={(e) => setUserData(prev => ({...prev,name:e.target.value}))}/>
         :<p className='font-medium text-3xl text-neutral-800 mt-4'>{userData.name}</p>
       }
 
@@ -37,13 +66,13 @@ const MyProfile = () => {
         <div className='grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700'>
           <p className='font-medium'>Email id: </p>
           <p className='text-blue-500'>{userData.email}</p>
-          <p className='font-medium'>Phone</p>
+          <p className='font-medium'>Phone:</p>
 
           {
-            isEdit? 
+            isEdit ? 
             <input 
             className='bg-gray-100 max-w-52'
-            type="text" value={userData.phone} onChange={e => setUserData(prev => ({...prev,phone:e.target.value}))}/>
+            type="text" value={userData.phone} onChange={(e) => setUserData(prev => ({...prev,phone:e.target.value}))}/>
             : <p className='text-blue-400'>{userData.phone}</p>
           }
           <p className='font-medium'>Address:</p>
@@ -52,19 +81,29 @@ const MyProfile = () => {
             ? <p>
               <input 
               className='bg-gray-50'
-              onChange={(e) => setUserData(prev => ({...prev,address,line1: e.target.value}))} value={userData.address.line1} type="text"/>
+              onChange={(e) => setUserData(prev => ({...prev,
+               address:{
+                ...prev.address,
+                line1: e.target.value
+                }
+                }))} value={userData.address.line1 || ""} type="text"/>
               <br/>
               <input 
               className='bg-gray-50'
-              onChange={(e) => setUserData(prev => ({...prev,address,line2: e.target.value}))} value={userData.address.line2} type="text"/>
+              onChange={(e) => setUserData(prev => ({...prev,
+              address: {
+                ...prev.address,
+                line2: e.target.value
+              }
+              }))} value={userData.address.line2 || ""} type="text"/>
             </p>
             : <p className='text-gray-500'>
               {
-                userData.address.line1
+                userData.address.line1 || "N/A"
               }
               <br/>
               {
-                userData.address.line2
+                userData.address.line2 || "N/A"
               }
             </p>
           }
@@ -99,7 +138,7 @@ const MyProfile = () => {
           isEdit
           ?<button 
           className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all'
-          onClick={()=>setIsEdit(false)}>Save Information</button>
+          onClick={updateUserProfileData}>Save Information</button>
           :<button 
           className='border border-primary px-8 py-2 rounded-full  hover:bg-primary hover:text-white transition-all'
           onClick={()=>setIsEdit(true)}>Edit</button>
