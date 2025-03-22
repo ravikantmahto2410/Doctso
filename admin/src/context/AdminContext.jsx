18387aa7d310886a2d2106575e1cd22e20b7af77
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-03-22 20:34:20 +0530

    make all pages in  admin panel

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -7,6 +7,7 @@ const AdminContextProvider = (props) => {
     const [aToken ,setAToken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
     const [doctors,setDoctors] = useState([])
     const [appointments,setAppointments] = useState([])
+    const [dashData, setDashData] = useState(false)
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const getAllDoctors = async () => {
@@ -69,13 +70,29 @@ const AdminContextProvider = (props) => {
         }
     }
 
+    const getDashData = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + '/api/admin/dashboard',{headers:{aToken}})
+
+            if(data.success){
+                setDashData(data.dashData)
+                console.log(data.dashData);
+                
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
 
     const value = {
         aToken,setAToken,
         backendUrl,doctors,
         getAllDoctors, changeAvailablity,
         appointments,setAppointments,
-        getAllAppointments,cancelAppointment
+        getAllAppointments,cancelAppointment,
+        dashData,getDashData
     }
     return (
         <AdminContext.Provider value={value}>
