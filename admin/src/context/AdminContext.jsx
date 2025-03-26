d85519462efb4ae69c8b044da4fcf21f90722dba
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-03-26 10:24:21 +0530

    backend Url provided

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -8,7 +8,7 @@ const AdminContextProvider = (props) => {
     const [doctors,setDoctors] = useState([])
     const [appointments,setAppointments] = useState([])
     const [dashData, setDashData] = useState(false)
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://doctso-backend.onrender.com"
 
     const getAllDoctors = async () => {
         try {
