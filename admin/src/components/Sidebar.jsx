32efa52c0928179f81cba4dd09fb2339c0106dfd
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-06-22 10:46:11 +0530

    backend ur; added on URL

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
                     md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4  border-[#5F6FFF]' : ''}`}
                     to={'/admin-dashboard'}>
                     <img src={assets.home_icon} alt="" />
-                    <p >Dashboard</p>
+                    <p>Dashboard</p>
                 </NavLink>
                 <NavLink 
                     className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9
