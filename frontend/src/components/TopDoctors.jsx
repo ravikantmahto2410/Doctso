b096b6443b4725a9cc4e5b35ba33f030fd9179de
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-06-26 11:35:33 +0530

    reload issue solved

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -11,6 +11,7 @@ const TopDoctors = () => {
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
         <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
+        <h2 className='text-3xl font-medium '>Create or Login to See Availability</h2>
         <p className='sm:w-1/3 text-center text-small'>Simply browse through our extensive list of trusted doctors</p>
         <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
         {doctors.slice(0,10).map((item,index) => (
